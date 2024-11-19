1792ba7997bfad8c97f49d188eefa6029ac9aea8
Momikung <chickenrty@gmail.com>
2024-11-19 03:12:06 +0000

    อันตรายจากไฟฟ้า

--- test_zoo.py
+++ test_zoo.py
@@ -7,7 +7,10 @@ class TestZoo(unittest.TestCase):
 
     def test_child_ticket_price(self):
         self.assertEqual(self.zoo.get_ticket_price(5), 50)
-       
+        self.assertEqual(self.zoo.get_ticket_price(20), 100)
+        self.assertEqual(self.zoo.get_ticket_price(30), 150)
+        self.assertEqual(self.zoo.get_ticket_price(70), 100)
+            
     # Add your additional test cases here.
 
 if __name__ == '__main__':
